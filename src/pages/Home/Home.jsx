9304f4bd41f912bd0555778398d86b7863ee79f9
yhmunna1 <yh.munna.barisal@gmail.com>
2024-09-24 20:19:00 +0600

    sswiperjs installed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,66 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+import banner1 from "../../assets/hero (1).jpg";
+import banner2 from "../../assets/hero (2).jpg";
+import banner3 from "../../assets/hero (3).jpg";
+import banner4 from "../../assets/hero (4).jpg";
 
 const Home = () => {
   return (
-    <div className="max-w-6xl mx-auto">
-      <h1 className="text-5xl">Welcome to Estate home page</h1>
+    <div className="">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={banner1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner4} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="hero min-h-screen"
+            style={{
+              backgroundImage: `url(${banner1})`,
+            }}
+          >
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-neutral-content text-center">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                <p className="mb-5">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                  assumenda excepturi exercitationem quasi. In deleniti eaque
+                  aut repudiandae et a id nisi.
+                </p>
+                <button className="btn btn-primary">Get Started</button>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
