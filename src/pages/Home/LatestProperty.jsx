c0949e21e5ca96a3567da06326c54ceeef25143d
yhmunna1 <yh.munna.barisal@gmail.com>
2024-09-27 22:53:36 +0600

    deafult 6 card feature added

--- src/pages/Home/LatestProperty.jsx
+++ src/pages/Home/LatestProperty.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import SingleProperty from "./SingleProperty";
 
 const LatestProperty = () => {
   const properties = useLoaderData();
+  const [visibleCount, setVisibleCount] = useState(6);
+
+  const handleLoadMore = () => {
+    setVisibleCount(properties.length);
+  };
+
   return (
     <div className="py-28 bg-slate-100">
       <div className="max-w-6xl mx-auto">
@@ -16,13 +22,27 @@ const LatestProperty = () => {
         </p>
 
         {/* Property */}
-        <div className="grid grid-col-1 md:grid-cols-3 gap-6">
-          {properties.map((property) => (
-            <SingleProperty
-              key={property.id}
-              property={property}
-            ></SingleProperty>
-          ))}
+        <div className="mx-auto">
+          <div className="grid grid-col-1 md:grid-cols-3 gap-6">
+            {properties.slice(0, visibleCount).map((property) => (
+              <SingleProperty
+                key={property.id}
+                property={property}
+              ></SingleProperty>
+            ))}
+          </div>
+
+          {/* Show Load More button only if there are more properties to show */}
+          {visibleCount < properties.length && (
+            <div className="text-center pt-5">
+              <button
+                onClick={handleLoadMore}
+                className="btn btn-warning border-red-500 bg-red-500 text-white"
+              >
+                Load More
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- src/pages/Home/SingleProperty.jsx
+++ src/pages/Home/SingleProperty.jsx
@@ -1,17 +1,60 @@
-import React from "react";
+import { MdSegment } from "react-icons/md";
+import { FaHouse } from "react-icons/fa6";
+import { PiMapPinSimpleAreaBold } from "react-icons/pi";
+import { IoLocationSharp } from "react-icons/io5";
+import { GrShareOption } from "react-icons/gr";
 
 const SingleProperty = ({ property }) => {
-  const { image, estate_title } = property;
+  const {
+    image,
+    estate_title,
+    description,
+    segment_name,
+    price,
+    status,
+    area,
+    location,
+    facilities,
+  } = property;
   return (
     <div className="card card-compact bg-base-100 w-96 border">
       <figure>
         <img src={image} alt="Shoes" />
       </figure>
       <div className="card-body">
+        <div className="flex items-center gap-1 text-gray-600">
+          <IoLocationSharp className="text-xl" />
+          <h3 className=" font-bold">{location}</h3>
+        </div>
         <h2 className="card-title">{estate_title}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <h4 className="font-bold text-red-500 text-base">{price}</h4>
+        <p className="text-gray-500">{description}</p>
+        <div className="flex gap-5">
+          <div className="flex items-center gap-1 text-gray-500">
+            <MdSegment />
+            <p className=" font-bold">{segment_name}</p>
+          </div>
+          <div className="flex items-center gap-1 text-gray-500">
+            <FaHouse />
+            <p className=" font-bold">{status}</p>
+          </div>
+          <div className="flex items-center gap-1 text-gray-500">
+            <PiMapPinSimpleAreaBold />
+            <p className=" font-bold">{area}</p>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center gap-1 text-gray-500">
+            <GrShareOption />
+            <p className=" font-bold">
+              {facilities[0]}, {facilities[1]}, {facilities[0]}
+            </p>
+          </div>
+        </div>
+        <div className="card-actions items-center">
+          <button className="btn btn-warning btn-sm border-red-500 bg-red-500 text-white">
+            View Property
+          </button>
         </div>
       </div>
     </div>
