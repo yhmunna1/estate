29d370368b80453a4a4c247a698a17cc29e1bc79
yhmunna1 <yh.munna.barisal@gmail.com>
2024-09-28 16:40:06 +0600

    reg page design

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Roots from "./layouts/Roots";
 import Home from "./pages/Home/Home";
 import Error from "./pages/Error/Error";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
       },
     ],
   },
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,20 +1,43 @@
 import { FaFacebook, FaGoogle } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
-const Login = () => {
-  const handleLogin = (e) => {
+const Register = () => {
+  const handleRegister = (e) => {
     e.preventDefault();
     const form = new FormData(e.currentTarget);
     const email = form.get("email");
     const password = form.get("password");
     console.log(email, password);
   };
-
   return (
     <div className="max-w-6xl mx-auto">
-      <div className="h-full border mt-32 py-10 px-28 rounded-lg">
-        <form onSubmit={handleLogin} className="card-body">
-          <h2 className="font-bold text-2xl">Login</h2>
+      <div className="h-full border mt-20 py-10 px-28 rounded-lg">
+        <form onSubmit={handleRegister} className="card-body">
+          <h2 className="font-bold text-2xl">Create an account</h2>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">First Name</span>
+            </label>
+            <input
+              type="text"
+              name="firstName"
+              placeholder="first name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Last Name</span>
+            </label>
+            <input
+              type="text"
+              name="lastName"
+              placeholder="last name"
+              className="input input-bordered"
+              required
+            />
+          </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
@@ -22,7 +45,7 @@ const Login = () => {
             <input
               type="email"
               name="email"
-              placeholder="jamu@na.com"
+              placeholder="email"
               className="input input-bordered"
               required
             />
@@ -34,18 +57,10 @@ const Login = () => {
             <input
               type="password"
               name="password"
-              placeholder="123456S"
+              placeholder="password"
               className="input input-bordered"
               required
             />
-            <label className="label">
-              <a
-                href="#"
-                className="label-text-alt link link-hover text-red-500 font-medium text-base"
-              >
-                Forgot password?
-              </a>
-            </label>
           </div>
           {/* {errorMessage && <p className="text-red-600">{errorMessage}</p>}
           {success && <p className="text-green-600">{success}</p>} */}
@@ -54,15 +69,15 @@ const Login = () => {
               type="submit"
               className="btn btn-error bg-red-500 text-white"
             >
-              Login
+              Create an account
             </button>
           </div>
         </form>
 
         <h5 className="text-base font-medium text-center">
-          Don’t have an account?{" "}
-          <Link to="/register" className="text-red-500">
-            Create an account
+          Already have an account?{" "}
+          <Link to="/login" className="text-red-500">
+            Login
           </Link>
         </h5>
       </div>
@@ -79,13 +94,11 @@ const Login = () => {
         </div>
         <div className="flex justify-center gap-4 border border-gray-300 w-1/3 mx-auto p-4 mb-12 rounded-full">
           <FaGoogle className="text-xl" />
-          <button className="font-medium text-base">
-            Continue with Google
-          </button>
+          <p className="font-medium text-base">Continue with Google</p>
         </div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
